49b64434a7c66123283c71d64c9b064cba85c9b9
Derek <derek.samson@etu.unice.fr>
2020-05-18 03:04:26 +0200

    changed app.get

--- HTML/iot-vue/server/node_lucioles_v2.js
+++ HTML/iot-vue/server/node_lucioles_v2.js
@@ -6,25 +6,24 @@ var path = require("path");
 
 const mqtt = require("mqtt");
 // Topics MQTT
-const TOPIC_LED ="sensors/led"
-const TOPIC_LIGHT ="sensors/light";
-const TOPIC_TEMP_TRESHOLD_1 ="tresholds/temp1";
-const TOPIC_TEMP_TRESHOLD_2 ="tresholds/temp2";
-const TOPIC_LIGHT_TRESHOLD_1 ="tresholds/light1";
-const TOPIC_LIGHT_TRESHOLD_2 ="tresholds/light2";
-const TOPIC_SLEEP_TIME_1 ="sleep_times/regime1";
-const TOPIC_SLEEP_TIME_2 ="sleep_times/regime2";
-const TOPIC_WORKING_HOURS_START_1 ="working_hours/start1";
-const TOPIC_WORKING_HOURS_END_1 ="working_hours/end1";
-const TOPIC_WORKING_HOURS_START_2 ="working_hours/start2";
-const TOPIC_WORKING_HOURS_END_2 ="working_hours/end2";
-const TOPIC_ALERT_TEMP ="alerts/temp";
-const TOPIC_ALERT_LIGHT ="alerts/light";
-
+const TOPIC_LED = "sensors/led";
+const TOPIC_LIGHT = "sensors/light";
+const TOPIC_TEMP_TRESHOLD_1 = "tresholds/temp1";
+const TOPIC_TEMP_TRESHOLD_2 = "tresholds/temp2";
+const TOPIC_LIGHT_TRESHOLD_1 = "tresholds/light1";
+const TOPIC_LIGHT_TRESHOLD_2 = "tresholds/light2";
+const TOPIC_SLEEP_TIME_1 = "sleep_times/regime1";
+const TOPIC_SLEEP_TIME_2 = "sleep_times/regime2";
+const TOPIC_WORKING_HOURS_START_1 = "working_hours/start1";
+const TOPIC_WORKING_HOURS_END_1 = "working_hours/end1";
+const TOPIC_WORKING_HOURS_START_2 = "working_hours/start2";
+const TOPIC_WORKING_HOURS_END_2 = "working_hours/end2";
+const TOPIC_ALERT_TEMP = "alerts/temp";
+const TOPIC_ALERT_LIGHT = "alerts/light";
 
 // Mailing
-process.env.NODE_TLS_REJECT_UNAUTHORIZED='0';
-    
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 // express
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -34,7 +33,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/")));
-app.use(function (request, response, next) {
+app.use(function(request, response, next) {
   //Pour eviter les problemes de CORS/REST
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "*");
@@ -60,7 +59,7 @@ const client = new MongoClient(uri, {
 });
 
 // Connection a la DB MongoDB
-client.connect(function (err, mongodbClient) {
+client.connect(function(err, mongodbClient) {
   if (err) throw err; // If connection to DB failed ...
   // else we get a "db" engine reference
 
@@ -69,7 +68,7 @@ client.connect(function (err, mongodbClient) {
   //
   var dbo = client.db(mongoBaseName);
 
- /* dbo.dropCollection("temp", function (err, delOK) {
+  /* dbo.dropCollection("temp", function (err, delOK) {
     if (err) throw err;
     if (delOK) console.log("Collection deleted");
   });
@@ -89,139 +88,145 @@ client.connect(function (err, mongodbClient) {
   //===============================================
   // Des la connection, le serveur NodeJS s'abonne aux topics MQTT
   //
-  client_mqtt.on("connect", function () {
-    client_mqtt.subscribe(TOPIC_LIGHT, function (err) {
+  client_mqtt.on("connect", function() {
+    client_mqtt.subscribe(TOPIC_LIGHT, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_LIGHT, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_LIGHT);
       }
     });
-    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_1, function (err) {
+    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_TEMP, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_TEMP_TRESHOLD_1);
       }
     });
-    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_2, function (err) {
+    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_2, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_TEMP, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_TEMP_TRESHOLD_2);
       }
     });
 
-    client_mqtt.subscribe(TOPIC_LED, function (err) {
+    client_mqtt.subscribe(TOPIC_LED, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_LED, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_LED);
       }
     });
 
-    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_1, function (err) {
+    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_TEMP_TRESHOLD_1, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_TEMP_TRESHOLD_1);
       }
     });
 
-    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_2, function (err) {
+    client_mqtt.subscribe(TOPIC_TEMP_TRESHOLD_2, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_TEMP_TRESHOLD_2, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_TEMP_TRESHOLD_2);
       }
     });
 
-     client_mqtt.subscribe(TOPIC_LIGHT_TRESHOLD_1, function (err) {
+    client_mqtt.subscribe(TOPIC_LIGHT_TRESHOLD_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_LIGHT_TRESHOLD_1, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_LIGHT_TRESHOLD_1);
       }
     });
 
-        client_mqtt.subscribe(TOPIC_LIGHT_TRESHOLD_2, function (err) {
-        if (!err) {
-          //client_mqtt.publish(TOPIC_LIGHT_TRESHOLD_2, 'Hello mqtt')
-          console.log("Node Server has subscribed to ", TOPIC_LIGHT_TRESHOLD_2);
-        }
-      });
+    client_mqtt.subscribe(TOPIC_LIGHT_TRESHOLD_2, function(err) {
+      if (!err) {
+        //client_mqtt.publish(TOPIC_LIGHT_TRESHOLD_2, 'Hello mqtt')
+        console.log("Node Server has subscribed to ", TOPIC_LIGHT_TRESHOLD_2);
+      }
+    });
 
-        client_mqtt.subscribe(TOPIC_SLEEP_TIME_1, function (err) {
+    client_mqtt.subscribe(TOPIC_SLEEP_TIME_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_SLEEP_TIME_1, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_SLEEP_TIME_1);
       }
     });
-        client_mqtt.subscribe(TOPIC_SLEEP_TIME_2, function (err) {
+    client_mqtt.subscribe(TOPIC_SLEEP_TIME_2, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_SLEEP_TIME_2, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_SLEEP_TIME_2);
       }
     });
 
-       client_mqtt.subscribe(TOPIC_WORKING_HOURS_START_1, function (err) {
+    client_mqtt.subscribe(TOPIC_WORKING_HOURS_START_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_WORKING_HOURS_START_1, 'Hello mqtt')
-        console.log("Node Server has subscribed to ", TOPIC_WORKING_HOURS_START_1);
+        console.log(
+          "Node Server has subscribed to ",
+          TOPIC_WORKING_HOURS_START_1
+        );
       }
-    }); 
-       client_mqtt.subscribe(TOPIC_WORKING_HOURS_END_1, function (err) {
+    });
+    client_mqtt.subscribe(TOPIC_WORKING_HOURS_END_1, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_WORKING_HOURS_END_1, 'Hello mqtt')
-        console.log("Node Server has subscribed to ", TOPIC_WORKING_HOURS_END_1);
+        console.log(
+          "Node Server has subscribed to ",
+          TOPIC_WORKING_HOURS_END_1
+        );
       }
-    }); 
-       client_mqtt.subscribe(TOPIC_WORKING_HOURS_START_2, function (err) {
+    });
+    client_mqtt.subscribe(TOPIC_WORKING_HOURS_START_2, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_WORKING_HOURS_START_2, 'Hello mqtt')
-        console.log("Node Server has subscribed to ", TOPIC_WORKING_HOURS_START_2);
-      }
-    }); 
-
-       client_mqtt.subscribe(TOPIC_WORKING_HOURS_END_2, function (err) {
-      if (!err) {
-        //client_mqtt.publish(TOPIC_WORKING_HOURS_END_2, 'Hello mqtt')
-        console.log("Node Server has subscribed to ", TOPIC_WORKING_HOURS_END_2);
+        console.log(
+          "Node Server has subscribed to ",
+          TOPIC_WORKING_HOURS_START_2
+        );
       }
     });
 
-    client_mqtt.subscribe(TOPIC_ALERT_TEMP, function (err) {
+    client_mqtt.subscribe(TOPIC_WORKING_HOURS_END_2, function(err) {
+      if (!err) {
+        //client_mqtt.publish(TOPIC_WORKING_HOURS_END_2, 'Hello mqtt')
+        console.log(
+          "Node Server has subscribed to ",
+          TOPIC_WORKING_HOURS_END_2
+        );
+      }
+    });
+
+    client_mqtt.subscribe(TOPIC_ALERT_TEMP, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_ALERT_TEMP, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_ALERT_TEMP);
       }
-    }); 
+    });
 
-    client_mqtt.subscribe(TOPIC_ALERT_LIGHT, function (err) {
+    client_mqtt.subscribe(TOPIC_ALERT_LIGHT, function(err) {
       if (!err) {
         //client_mqtt.publish(TOPIC_ALERT_LIGHT, 'Hello mqtt')
         console.log("Node Server has subscribed to ", TOPIC_ALERT_LIGHT);
       }
-    }); 
- 
-
+    });
   });
 
-  
-
-app.post('/sendRegime', function(request, response) {
-  console.log('POST /')
-  console.dir(request.body)
-  if(request.id == 1) {
-    //client_mqtt.publish()
-  } else if(request.id == 2) {
-    //client_mqtt.publish()
-  }
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end('thanks')
-})
-
-
+  app.post("/sendRegime", function(request, response) {
+    console.log("POST /");
+    console.dir(request.body);
+    if (request.id == 1) {
+      //client_mqtt.publish()
+    } else if (request.id == 2) {
+      //client_mqtt.publish()
+    }
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("thanks");
+  });
 
   //================================================================
   // Callback de la reception des messages MQTT pour les topics sur
   // lesquels on s'est inscrit.
   // C'est cette fonction qui alimente la BD.
   //
-  client_mqtt.on("message", function (topic, message) {
+  client_mqtt.on("message", function(topic, message) {
     console.log("MQTT msg on topic : ", topic.toString());
     console.log("Msg payload : ", message.toString());
 
@@ -230,53 +235,57 @@ app.post('/sendRegime', function(request, response) {
     wh = message.who;
     val = message.value;
 
-    
     //Envoi du mail en fonction de l'alerte reçue
     // VARIABLES Mailing
     let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 587,
       secure: false,
       requireTLS: true,
       auth: {
-          user: 'alert.lucioles@gmail.com',
-          pass: 'iotM1Miage'
-      }
-  });
-  
-  let mailAlerteLuminosité = {
-      from: 'alert.lucioles@gmail.com',
-      to: 'redadu06.rj@gmail.com',
-      subject: 'Test',
-      text: 'Bonjour, \n Ceci est une alerte de luminosté. La luminosité est de '+val+'\n Une ou plusieurs lampes de votre domicile sont allumées alors qu elles ne devraient pas l être'
-  };
-  
-  let mailAlerteTemperature = {
-    from: 'alert.lucioles@gmail.com',
-    to: 'redadu06.rj@gmail.com',
-    subject: 'Test',
-    text: 'Bonjour,  \n Ceci est une alerte de température. La température est de '+val+'°C. \n La température de votre domicile dépasse la normale, veuillez vérifier cette dernière'
-  };
-  
-    if(topic == TOPIC_ALERT_TEMP){
+        user: "alert.lucioles@gmail.com",
+        pass: "iotM1Miage",
+      },
+    });
+
+    let mailAlerteLuminosité = {
+      from: "alert.lucioles@gmail.com",
+      to: "redadu06.rj@gmail.com",
+      subject: "Test",
+      text:
+        "Bonjour, \n Ceci est une alerte de luminosté. La luminosité est de " +
+        val +
+        "\n Une ou plusieurs lampes de votre domicile sont allumées alors qu elles ne devraient pas l être",
+    };
+
+    let mailAlerteTemperature = {
+      from: "alert.lucioles@gmail.com",
+      to: "redadu06.rj@gmail.com",
+      subject: "Test",
+      text:
+        "Bonjour,  \n Ceci est une alerte de température. La température est de " +
+        val +
+        "°C. \n La température de votre domicile dépasse la normale, veuillez vérifier cette dernière",
+    };
+
+    if (topic == TOPIC_ALERT_TEMP) {
       transporter.sendMail(mailAlerteLuminosité, (error, info) => {
         if (error) {
-            return console.log(error.message);
+          return console.log(error.message);
         }
-        console.log('Envoi mail alerte temperature');
-    });
-    }
-
-    if(topic == TOPIC_ALERT_LIGHT){
-      transporter.sendMail(mailAlerteTemperature, (error, info) => {
-        if (error) {
-            return console.log(error.message);
-        }
-        console.log('success');
+        console.log("Envoi mail alerte temperature");
       });
     }
 
-    
+    if (topic == TOPIC_ALERT_LIGHT) {
+      transporter.sendMail(mailAlerteTemperature, (error, info) => {
+        if (error) {
+          return console.log(error.message);
+        }
+        console.log("success");
+      });
+    }
+
     // Debug : Gerer une liste de who pour savoir qui utilise le node server
     let wholist = [];
     var index = wholist.findIndex((x) => x.who == wh);
@@ -306,14 +315,14 @@ app.post('/sendRegime', function(request, response) {
     // Stocker la donnee/value contenue dans le message en
     // utilisant le nom du topic comme key dans la BD
     key = topicname;
-    dbo.collection(key).insertOne(new_entry, function (err, res) {
+    dbo.collection(key).insertOne(new_entry, function(err, res) {
       if (err) throw err;
       console.log("Item inserted in db in collection :", key);
       console.log(new_entry);
     });
 
     // Debug : voir les collections de la DB
-    dbo.listCollections().toArray(function (err, collInfos) {
+    dbo.listCollections().toArray(function(err, collInfos) {
       // collInfos is an array of collection info objects that look like:
       // { name: 'test', options: {} }
       console.log("\nList of collections currently in DB: ", collInfos);
@@ -333,10 +342,10 @@ app.post('/sendRegime', function(request, response) {
   //================================================================
   //==== REQUETES HTTP reconnues par le Node =======================
   //================================================================
-
+  app.use("/static", express.static(__dirname + "/public"));
   // Accés par le Node a la page HTML affichant les charts
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/ui_lucioles.html"));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
   });
 
   // Function for answering GET request on this node server ...
@@ -345,7 +354,7 @@ app.post('/sendRegime', function(request, response) {
   //     /esp/temp?who=80%3A7D%3A3A%3AFD%3AC9%3A44
   // Utilisation de routes dynamiques => meme fonction pour
   // /esp/temp et /esp/light
-  app.get("/esp/:what", function (req, res) {
+  app.get("/esp/:what", function(req, res) {
     // cf https://stackabuse.com/get-query-strings-and-parameters-in-express-js/
     console.log(req.originalUrl);
 
@@ -369,7 +378,7 @@ app.post('/sendRegime', function(request, response) {
       .find({ who: wh })
       .sort({ _id: -1 })
       .limit(nb)
-      .toArray(function (err, result) {
+      .toArray(function(err, result) {
         if (err) throw err;
         console.log("get on ", key);
         console.log(result);
@@ -382,12 +391,13 @@ app.post('/sendRegime', function(request, response) {
 
 // L'application est accessible sur le port 3000
 app.listen(3000, () => {
-  console.log("Server listening on port 3000"); 
+  console.log("Server listening on port 3000");
 });
 
-app.post('/s', (req,res)=>{
-  gm(img_path).implode(-1.2).write(op_path, function(err) {
-      if (err)
-          console.log(err);
-  })
+app.post("/s", (req, res) => {
+  gm(img_path)
+    .implode(-1.2)
+    .write(op_path, function(err) {
+      if (err) console.log(err);
+    });
 });
